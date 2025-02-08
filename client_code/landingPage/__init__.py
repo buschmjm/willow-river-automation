a1de6e8159883a27c1b7fb74221f7a4623b8a7ed
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2025-02-08 23:56:57 +0000

    Edited staticLanding.html and landingPage; Deleted new_file.html, home and landingPage.RowTemplate1

--- client_code/landingPage/__init__.py
+++ client_code/landingPage/__init__.py
@@ -1,24 +1,10 @@
 from ._anvil_designer import landingPageTemplate
 from anvil import *
 
-class landingPage(landingPageTemplate):
-    def __init__(self, **properties):
-        self.init_components(**properties)
 
-    def form_show(self, **event_args):
-        """This method is called when the form is shown on the page"""
-        # Set the HTML content directly to the HTML component
-        self.content_html.html = """
-        <div class="landing-content">
-            <div class="header landing-header">
-                <h1>Willow River Automation Services</h1>
-                <h2>Streamlining Workflows, Automating Success.</h2>
-            </div>
-            <div class="hero">
-                <h1>Optimize your business with cutting-edge automation solutions.</h1>
-                <p>Discover Our Services!</p>
-                <button>Get Started</button>
-            </div>
-            <!-- ... rest of your HTML content ... -->
-        </div>
-        """
+class landingPage(landingPageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
