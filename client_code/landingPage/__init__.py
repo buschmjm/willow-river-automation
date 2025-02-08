ce8af2d6bf60de42bccbf760abb18bd17e1ab80f
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2025-02-08 22:11:22 +0000

    Created LICENSE.txt; Edited settings and landingPage

--- client_code/landingPage/__init__.py
+++ client_code/landingPage/__init__.py
@@ -1,18 +1,19 @@
 from ._anvil_designer import landingPageTemplate
 from anvil import *
 
-
 class landingPage(landingPageTemplate):
-  def __init__(self, **properties):
+    def __init__(self, **properties):
         self.init_components(**properties)
 
         # Set background image dynamically
         self.set_background_image()
 
-  def set_background_image(self):
-        # Apply background image from the assets folder
+    def set_background_image(self):
+        # Apply background image to the MAIN FORM
         self.role = "background"
-        self.style["backgroundImage"] = "url('_/theme/laptop-593673.jpg')"
-        self.style["backgroundSize"] = "cover"
-        self.style["backgroundPosition"] = "center"
-        self.style["backgroundRepeat"] = "no-repeat"
+
+        # Apply background styling to a specific panel (e.g., `column_panel_1`)
+        self.outlined_card_1.style["backgroundImage"] = "url('_/theme/laptop-593673.jpg')"
+        self.outlined_card_1.style["backgroundSize"] = "cover"
+        self.outlined_card_1.style["backgroundPosition"] = "center"
+        self.outlined_card_1.style["backgroundRepeat"] = "no-repeat"
