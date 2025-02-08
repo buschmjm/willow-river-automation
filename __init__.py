3901b00634331c71df86eceb3b4fb1b0e9cad1cd
Jacob Buschmann <44792676+buschmjm@users.noreply.github.com>
2025-02-08 22:43:15 +0000

    Add landing page implementation with styles and navigation

--- __init__.py
+++ __init__.py
@@ -5,3 +5,11 @@
 # python -m anvil.run_app_via_uplink YourAppPackageName
 
 __path__ = [__path__[0] + "/server_code", __path__[0] + "/client_code"]
+
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+from anvil.tables import app_tables
+
+def show_landing_page():
+    open_form('landingPage')
