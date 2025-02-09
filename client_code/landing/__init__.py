7061bd231a8a6b77cbb0aa8423fcce6cc5f569ce
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2025-02-09 00:13:09 +0000

    Created landing; Edited settings; Deleted staticLanding.html, landingPage and Home

--- client_code/landing/__init__.py
+++ client_code/landing/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import landingPageTemplate
+from ._anvil_designer import landingTemplate
 from anvil import *
 
 
-class landingPage(landingPageTemplate):
+class landing(landingTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
